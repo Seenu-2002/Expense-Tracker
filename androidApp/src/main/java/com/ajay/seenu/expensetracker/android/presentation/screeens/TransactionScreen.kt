9a733ba9b183a2cc0d75a9dd105c04d89b9a2595
Ajay Chandran <ajaychandran.r@zohocorp.com>
2025-08-12 18:44:52 +0530

    minor changes

--- androidApp/src/main/java/com/ajay/seenu/expensetracker/android/presentation/screeens/TransactionScreen.kt
+++ androidApp/src/main/java/com/ajay/seenu/expensetracker/android/presentation/screeens/TransactionScreen.kt
@@ -6,29 +6,21 @@ import android.widget.Toast
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.TweenSpec
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -38,12 +30,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
@@ -52,19 +41,18 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavController
 import com.ajay.seenu.expensetracker.android.R
 import com.ajay.seenu.expensetracker.android.data.TransactionMode
 import com.ajay.seenu.expensetracker.android.domain.CategoryRelationMapper
 import com.ajay.seenu.expensetracker.android.domain.data.Transaction
 import com.ajay.seenu.expensetracker.android.presentation.viewmodels.AddTransactionViewModel
-import com.ajay.seenu.expensetracker.android.presentation.widgets.AddTransactionForm
+import com.ajay.seenu.expensetracker.android.presentation.widgets.TransactionForm
 import com.ajay.seenu.expensetracker.entity.PaymentType
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddTransactionScreen(
+fun TransactionScreen(
     transactionMode: TransactionMode = TransactionMode.New,
     viewModel: AddTransactionViewModel = hiltViewModel(),
     onNavigateBack: () -> Unit
@@ -124,9 +112,10 @@ fun AddTransactionScreen(
     }
 
     if(showForm) {
-        AddTransactionForm(
+        TransactionForm(
             modifier = Modifier
                 .padding(horizontal = 48.dp, vertical = 32.dp),
+            transactionMode = transactionMode,
             transaction = transaction,
             existingAttachments = attachments,
             onCategoryClicked = {
