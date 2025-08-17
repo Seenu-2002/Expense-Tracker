46c244e929ccc63b8990035d467d7f0e3eaddd52
Seenivasan T <misterseenivasan2002@gmail.com>
2025-08-17 12:57:47 +0530

    - Resolved merge conflicts

--- androidApp/src/main/java/com/ajay/seenu/expensetracker/android/presentation/screeens/TransactionScreen.kt
+++ androidApp/src/main/java/com/ajay/seenu/expensetracker/android/presentation/screeens/TransactionScreen.kt
@@ -192,8 +192,7 @@ fun CategoryBottomSheet(
                     category,
                     iconBoxSize = 42.dp,
                     clickable = true,
-                    iconSize = 20.dp,
-                    clickable = true
+                    iconSize = 20.dp
                 ) { selectedCategory ->
                     onCategorySelected(selectedCategory)
                 }
